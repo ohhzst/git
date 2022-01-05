3188dd0751700612f89673e2addd34925bf26d82
ZST <791737319@qq.com>
2022-01-05 17:52:39 +0800

    first commit

---
+++ src/main/java/com/pcy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.pcy.git;
+
+/**
+ * @author zst
+ * @create 2022-01-05 17:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+}
